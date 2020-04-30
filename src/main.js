2ed05905be1a73b85060cc6c8245199fba58c298
David Boedigheimer <stealth259@gmail.com>
2020-04-30 16:04:01 -0700

    Add png import.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import { ApiHelper } from './ApiHelper';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import png from './img/mainBackground.png';
 
 let service = new ApiHelper();
 
