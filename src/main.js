a0781c115aab163e88153e9c82fe1454319aa618
David Boedigheimer <stealth259@gmail.com>
2020-05-14 15:23:49 -0700

    Create branch for grid snappable objects.

--- src/main.js
+++ src/main.js
@@ -14,6 +14,30 @@ $(document).ready(function() {
     $('.parent').show();
     $('#attack').show();
     $('.cards').show();
+
+    const containerSelector = '#Snappable .BlockLayout';
+    const containers = document.querySelectorAll(containerSelector);
+
+    if (containers.length === 0) {
+      return false;
+    }
+
+    const swappable = new Swappable(containers, {
+      mirror: {
+        appendTo: containerSelector,
+        constrainDimensions: true,
+      },
+      plugins: [Plugins.Snappable],
+    });
+
+    // --- Draggable events --- //
+    swappable.on('drag:start', (evt) => {
+      if (evt.originalSource.classList.contains('Block--typeStripes')) {
+        evt.cancel();
+      }
+    });
+
+    return swappable;
   });
 
   // let data = service.Acquire();
