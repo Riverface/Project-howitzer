eda573295fd4aeef4c947ddc5c62a41db8a43a0e
Riverface <sonoskeakigawa@gmail.com>
2020-05-14 09:20:05 -0700

    Fixed some things

--- src/main.js
+++ src/main.js
@@ -7,27 +7,18 @@ import png from './img/mainBackground.png';
 
 $(document).ready(function() {
 
-  $("#start-button").click(function () {
-    $('.jumbotron').slideUp();
-    $('#start-button').slideUp();
-  });
+    $("#start-button").click(function() {
+        $('.jumbotron').slideUp();
+        $('#start-button').slideUp();
+    });
 
-  let data = service.Acquire();
-	/*for (let i = 0; i < data.length; i++) {
-	   $(".output").append(data[i]);
-  }*/
-  data.then(function(response) {
-    console.log(response);
+    let game = new Game();
 
+    $("button").on("click", function() {
+        console.log(game.allCards, "game.allCards");
 
-  let game = new Game();
-
-  $("button").on("click", function() {
-    console.log(game.allCards, "game.allCards");
-
-  	for (let i = 0; i < game.allCards.length; i++) {
-  	   $(".output").append(game.allCards[i].name);
-    }
-  });
-
+        for (let i = 0; i < game.allCards.length; i++) {
+            $(".output").append(game.allCards[i].name);
+        }
+    });
 });
